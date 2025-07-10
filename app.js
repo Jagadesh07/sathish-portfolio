dc6463044cdee1f46afe835dfefec27064cf68d8
Jagadesh07 <86509073+Jagadesh07@users.noreply.github.com>
2025-07-10 16:25:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,3 +34,46 @@ function revealFunction(){
 window.addEventListener('load',() => {
     revealFunction();
 });
+
+// darkmode
+
+const darkMode = document.querySelector('.dark');
+const lightMode = document.querySelector('.light');
+let dark = false;
+
+darkMode.addEventListener('click',() => {
+    dark = true;
+    darkMode.style.backgroundColor = 'black';
+    darkMode.style.color = 'white';
+    lightMode.style.backgroundColor = 'rgba(15, 15, 15, 0.11)';
+    lightMode.style.color = 'white';
+    setMode();
+})
+lightMode.addEventListener('click',() => {
+    dark = false;
+    lightMode.style.backgroundColor = 'white';
+    lightMode.style.color = 'black';
+    darkMode.style.backgroundColor = 'rgb(223, 234, 240)';
+    darkMode.style.color = 'black';
+    setMode();
+})
+
+
+let r = document.querySelector(':root');
+
+function setMode(){
+    if(dark){
+    r.style.setProperty('--body-background','rgb(36, 36, 36)');
+    r.style.setProperty('--text-color','white');
+    r.style.setProperty('--highlight-color','rgb(29, 27, 27)');
+    r.style.setProperty('--nav-background','rgba(15, 15, 15, 0.25)');
+    r.style.setProperty('--nav-active','black');
+}
+else{
+    r.style.setProperty('--body-background','rgb(235, 242, 245)');
+    r.style.setProperty('--text-color','black');
+    r.style.setProperty('--highlight-color','white');
+    r.style.setProperty('--nav-background','rgb(223, 234, 240)');
+    r.style.setProperty('--nav-active','white');
+}
+}
